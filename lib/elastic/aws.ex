0cc3e007eb69b0dec03e44d63c795e4400b84a18
Michael Clifford <cliffom@gmail.com>
2017-07-19 23:22:27 -0700

    Removed deprecated code and refactored how signed headers are built

--- lib/elastic/aws.ex
+++ lib/elastic/aws.ex
@@ -14,26 +14,13 @@ defmodule Elastic.AWS do
       settings().region,
       "es"
     )
-    |> Enum.into(%{})
     |> build_signed_headers
   end
 
-  # DELETE requests do not support headers
-  defp process_headers(:delete, _), do: %{}
-
-  defp process_headers(_method, headers) do
-    for {k, v} <- headers,
-      into: %{},
-      do: {to_string(k), to_string(v)}
-  end
-
   defp build_signed_headers(headers) do
-    %{
-      "authorization" => authorization,
-      "x-amz-date" => x_amz_date,
-      "x-amz-content-sha256" => x_amz_content_sha256
-    } = headers
-    ["Authorization": authorization, "X-Amz-Date": x_amz_date, "X-Amz-Content-Sha256": x_amz_content_sha256]
+    headers
+    |> Enum.into(%{})
+    |> Enum.map(fn({key, value}) -> {:"#{key}", value} end)
   end
 
   defp settings do
